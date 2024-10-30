be395af573c00efc041e649e19b9b6cec33dcc01
VinceeCarterr <220711806@students.uajy.ac.id>
2024-10-30 16:00:48 +0700

    update FnB, Home (now playing + coming soon)

--- lib/fnb.dart
+++ lib/fnb.dart
@@ -7,20 +7,62 @@ class FnBPage extends StatefulWidget {
   _FnBPageState createState() => _FnBPageState();
 }
 
-class _FnBPageState extends State<FnBPage> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
+class _FnBPageState extends State<FnBPage> {
+  int _selectedIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
+  final List<Item> _bundles = [
+    Item(
+        title: 'Atma Bundle 1',
+        description: 'Atma Popcorn Small + Soft Drink Small',
+        price: 'Rp 45.000'),
+    Item(
+        title: 'Atma Bundle 2',
+        description: 'Atma Popcorn Medium + Soft Drink Small',
+        price: 'Rp 60.000'),
+    Item(
+        title: 'Atma Bundle 3',
+        description: 'Atma Popcorn Large + Soft Drink Small',
+        price: 'Rp 75.000'),
+    Item(
+        title: 'Atma Bundle 4',
+        description: 'Atma Snack Platter + Atma Tea',
+        price: 'Rp 100.000'),
+  ];
 
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
+  final List<Item> _foods = [
+    Item(
+        title: 'Atma Popcorn Small',
+        description: 'Small-sized popcorn',
+        price: 'Rp 30.000'),
+    Item(
+        title: 'Atma Popcorn Medium',
+        description: 'Medium-sized popcorn',
+        price: 'Rp 45.000'),
+    Item(
+        title: 'Atma Popcorn Large',
+        description: 'Large-sized popcorn',
+        price: 'Rp 60.000'),
+    Item(
+        title: 'Atma Snack Platter',
+        description: 'Delicious snack platter',
+        price: 'Rp 70.000'),
+  ];
+
+  final List<Item> _drinks = [
+    Item(
+        title: 'Soft Drink Small',
+        description: 'Refreshing soft drink',
+        price: 'Rp 15.000'),
+    Item(
+        title: 'Soft Drink Large',
+        description: 'Large refreshing drink',
+        price: 'Rp 25.000'),
+    Item(title: 'Milo', description: 'Medium Sized Milo', price: 'Rp 35.000'),
+    Item(
+        title: 'Atma Tea',
+        description: 'Authentic Atma tea',
+        price: 'Rp 20.000'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +87,7 @@ class _FnBPageState extends State<FnBPage> with SingleTickerProviderStateMixin {
                     const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: const Color(0x1F1F1F),
+                    color: const Color(0xFF1F1F1F),
                     borderRadius: BorderRadius.circular(24.0),
                     border: Border.all(color: Colors.white, width: 1),
                   ),
@@ -54,7 +96,7 @@ class _FnBPageState extends State<FnBPage> with SingleTickerProviderStateMixin {
                       IconButton(
                         icon: const Icon(Icons.search, color: Colors.white54),
                         onPressed: () {
-                          //buat search nanti
+                          // buat search
                         },
                       ),
                       Expanded(
@@ -68,28 +110,32 @@ class _FnBPageState extends State<FnBPage> with SingleTickerProviderStateMixin {
                       IconButton(
                         icon: const Icon(Icons.mic, color: Colors.white54),
                         onPressed: () {
-                          // buat voice search nanti
+                          // buat voice search
                         },
                       ),
                     ],
                   ),
                 ),
               ),
-              TabBar(
-                controller: _tabController,
-                indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    _buildToggleButton("Bundle", 0),
+                    const SizedBox(width: 8),
+                    _buildToggleButton("Food", 1),
+                    const SizedBox(width: 8),
+                    _buildToggleButton("Drink", 2),
+                    const Spacer(),
+                    IconButton(
+                      icon: const Icon(Icons.swap_vert, color: Colors.white),
+                      onPressed: () {
+                        // buat sort?
+                      },
+                    ),
+                  ],
                 ),
-                labelPadding: const EdgeInsets.symmetric(horizontal: 16.0),
-                tabs: [
-                  _buildTab("Bundle", isSelected: _tabController.index == 0),
-                  _buildTab("Food", isSelected: _tabController.index == 1),
-                  _buildTab("Drink", isSelected: _tabController.index == 2),
-                ],
-                onTap: (index) {
-                  setState(() {});
-                },
               ),
             ],
           ),
@@ -104,37 +150,50 @@ class _FnBPageState extends State<FnBPage> with SingleTickerProviderStateMixin {
             stops: [0.3, 0.7],
           ),
         ),
-        child: TabBarView(
-          controller: _tabController,
-          children: [
-            _buildItemList(_bundles),
-            _buildItemList(_foods),
-            _buildItemList(_drinks),
-          ],
+        child: _buildItemList(),
+      ),
+    );
+  }
+
+  Widget _buildToggleButton(String label, int index) {
+    bool isSelected = _selectedIndex == index;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.white : Colors.black,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.white, width: 1.5),
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? Colors.black : Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildTab(String label, {required bool isSelected}) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      decoration: BoxDecoration(
-        color: isSelected ? Colors.white : Colors.black,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.white, width: 1.5),
-      ),
-      child: Text(
-        label,
-        style: TextStyle(
-          color: isSelected ? Colors.black : Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-  }
+  Widget _buildItemList() {
+    List<Item> items;
+    switch (_selectedIndex) {
+      case 1:
+        items = _foods;
+        break;
+      case 2:
+        items = _drinks;
+        break;
+      default:
+        items = _bundles;
+    }
 
-  Widget _buildItemList(List<Item> items) {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemCount: items.length,
@@ -160,55 +219,59 @@ class FnBItemCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(16.0),
           border: Border.all(color: Colors.white, width: 1),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.asset(
-                  'images/bg4.jpg',
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(width: 14),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      item.title,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.asset(
+                      'images/bg4.jpg',
+                      width: 90,
+                      height: 90,
+                      fit: BoxFit.cover,
                     ),
-                    const SizedBox(height: 6),
-                    Text(
-                      item.description,
-                      style: const TextStyle(
-                        color: Colors.white70,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Text(
-                  item.price,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
                   ),
+                  const SizedBox(width: 14),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item.title,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          item.description,
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 8,
+              right: 8,
+              child: Text(
+                item.price,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -222,55 +285,3 @@ class Item {
 
   Item({required this.title, required this.description, required this.price});
 }
-
-final List<Item> _bundles = [
-  Item(
-      title: 'Atma Bundle 1',
-      description: 'Atma Popcorn Small + Soft Drink Small',
-      price: 'Rp 45.000'),
-  Item(
-      title: 'Atma Bundle 2',
-      description: 'Atma Popcorn Medium + Soft Drink Small',
-      price: 'Rp 60.000'),
-  Item(
-      title: 'Atma Bundle 3',
-      description: 'Atma Popcorn Large + Soft Drink Small',
-      price: 'Rp 75.000'),
-  Item(
-      title: 'Atma Bundle 4',
-      description: 'Atma Snack Platter + Atma Tea',
-      price: 'Rp 100.000'),
-];
-
-final List<Item> _foods = [
-  Item(
-      title: 'Atma Popcorn Small',
-      description: 'Small-sized popcorn',
-      price: 'Rp 30.000'),
-  Item(
-      title: 'Atma Popcorn Medium',
-      description: 'Medium-sized popcorn',
-      price: 'Rp 45.000'),
-  Item(
-      title: 'Atma Popcorn Large',
-      description: 'Large-sized popcorn',
-      price: 'Rp 60.000'),
-  Item(
-      title: 'Atma Snack Platter',
-      description: 'Delicious snack platter',
-      price: 'Rp 70.000'),
-];
-
-final List<Item> _drinks = [
-  Item(
-      title: 'Soft Drink Small',
-      description: 'Refreshing soft drink',
-      price: 'Rp 15.000'),
-  Item(
-      title: 'Soft Drink Large',
-      description: 'Large refreshing drink',
-      price: 'Rp 25.000'),
-  Item(title: 'Milo', description: 'Medium Sized Milo', price: 'Rp 35.000'),
-  Item(
-      title: 'Atma Tea', description: 'Authentic Atma tea', price: 'Rp 20.000'),
-];
