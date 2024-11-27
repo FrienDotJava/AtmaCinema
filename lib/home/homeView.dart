082b87851f32e4f8c55acb636ae68ccd03bc0c20
VinceeCarterr <220711806@students.uajy.ac.id>
2024-11-27 12:02:41 +0700

    news + top movie

--- lib/home/homeView.dart
+++ lib/home/homeView.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:tubes/movie/movie_list.dart';
+import 'package:tubes/movie/movie.dart';
+import 'package:tubes/movie/movieDetail.dart';
+import 'package:tubes/movie/topMovieList.dart';
 import 'package:tubes/home/home.dart';
 import 'package:tubes/profile/profile.dart';
+import 'package:tubes/news/news_list.dart';
+import 'package:tubes/news/news_detail.dart';
 
 class MyHomeView extends StatefulWidget {
   const MyHomeView({super.key});
@@ -24,25 +29,33 @@ class _MyHomeViewState extends State<MyHomeView> {
             stops: [0.3, 0.7],
           ),
         ),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            _buildSearchSection(context),
-            const SizedBox(height: 10),
-            _buildMovieSection(
-              context,
-              title: 'Now Playing',
-              movies: _buildNowPlayingMovies(),
-              nowPlaying: true,
-            ),
-            const SizedBox(height: 12),
-            _buildMovieSection(
-              context,
-              title: 'Coming Soon',
-              movies: _buildComingSoonMovies(),
-              nowPlaying: false,
-            ),
-          ],
+        child: SingleChildScrollView(
+          // Enables vertical scrolling
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              _buildSearchSection(context),
+              const SizedBox(height: 10),
+              _buildMovieSection(
+                context,
+                title: 'Now Playing',
+                movies: _buildNowPlayingMovies(),
+                nowPlaying: true,
+              ),
+              const SizedBox(height: 12),
+              _buildMovieSection(
+                context,
+                title: 'Coming Soon',
+                movies: _buildComingSoonMovies(),
+                nowPlaying: false,
+              ),
+              const SizedBox(height: 30),
+              buildAtmaNewsSection(context),
+              const SizedBox(height: 30),
+              buildTopMoviesSection(context),
+              const SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
@@ -357,6 +370,273 @@ class ComingSoonCard extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
             textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget buildAtmaNewsSection(BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'ATMA news',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NewsList()),
+                );
+              },
+              child: const Text(
+                'See all >',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 12),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            AtmaNewsCard(
+              imagePath: 'images/bg2.jpg',
+              description:
+                  'Spider-Man: Way Back Home Confirmed for 2025 Release!',
+            ),
+            AtmaNewsCard(
+              imagePath: 'images/bg2.jpg',
+              description:
+                  'Robert Downey Jr. Officially Returns as Iron Man: Announcement Delights Marvel Fans!',
+            ),
+            AtmaNewsCard(
+              imagePath: 'images/bg2.jpg',
+              description:
+                  'Chris Evans Wields the Shield Again: Marvel Fans Cheer for the Return of Captain America!',
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+// ATMA News Card Widget
+class AtmaNewsCard extends StatelessWidget {
+  final String imagePath;
+  final String description;
+
+  const AtmaNewsCard({
+    required this.imagePath,
+    required this.description,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NewsDetail(
+              imagePath: imagePath,
+              title: description,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 16.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+                width: 80,
+                height: 80,
+              ),
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Text(
+                description,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildTopMoviesSection(BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Top Movies For You!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TopMovieList()),
+                );
+              },
+              child: const Text(
+                'See all >',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 12),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            MovieListCard(
+              imagePath: 'images/bg2.jpg',
+              title: 'AVENGERS: ENDGAME',
+              duration: '1h 20m',
+              rating: '5/5',
+              ageRating: '17+',
+              format: '2D',
+            ),
+            MovieListCard(
+              imagePath: 'images/bg2.jpg',
+              title: 'AVENGERS: AGE OF ULTRON',
+              duration: '1h 20m',
+              rating: '4.9/5',
+              ageRating: '17+',
+              format: '2D',
+            ),
+            MovieListCard(
+              imagePath: 'images/bg2.jpg',
+              title: 'CAPTAIN AMERICA: CIVIL WAR',
+              duration: '1h 20m',
+              rating: '4.9/5',
+              ageRating: '17+',
+              format: '2D',
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+class MovieListCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String duration;
+  final String rating;
+  final String ageRating;
+  final String format;
+
+  const MovieListCard({
+    required this.imagePath,
+    required this.title,
+    required this.duration,
+    required this.rating,
+    required this.ageRating,
+    required this.format,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: Image.asset(
+              imagePath,
+              fit: BoxFit.cover,
+              width: 100,
+              height: 120,
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    MovieTag(text: duration),
+                    const SizedBox(width: 8),
+                    MovieTag(text: ageRating),
+                    const SizedBox(width: 8),
+                    MovieTag(text: format),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    const Icon(Icons.star, color: Colors.yellow, size: 16),
+                    const SizedBox(width: 4),
+                    Text(
+                      rating,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
